109fd22ec9b796bc9310875389223c527d07f258
DJGilmore4U <daniellejgilmore@gmail.com>
2018-04-17 20:22:18 -0700

    added inventory

--- bamazon.sql
+++ bamazon.sql
@@ -13,15 +13,33 @@ CREATE TABLE products(
 );
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("000001", "Avocado", "Produce", 1.99, 1000);
+VALUES ("0001", "Avocado", "Produce", 5, 1000);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("000002", "Lime", "Produce", .25, 1000);
+VALUES ("0002", "Lime", "Produce", 2, 1000);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("000003", "Orange", "Produce", .59, 1000);
+VALUES ("0003", "Orange", "Produce", 3, 1000);
 
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
-VALUES ("000004", "Cilantro", "Produce", 1.25, 1000);
+VALUES ("0004", "Cilantro", "Produce", 1, 1000);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("0005", "Baguette", "Bakery", 4, 1000);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("0006", "Porterhouse", "Butcher", 25, 1000);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("0007", "Salmon", "SeaFood", 15, 1000);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("0008", "Cherries", "Produce", 6, 1000);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("0009", "Tequila", "Liquor", 35, 1000);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES ("00010", "FroYo", "Frozen", 7, 1000);
 
 
